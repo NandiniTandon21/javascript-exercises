09931d83ad1924b8f20635039c5c44085d409225
NandiniTandon21 <63950748+NandiniTandon21@users.noreply.github.com>
2025-01-09 01:10:35 +0530

    first and second

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,23 @@
-const repeatString = function() {
+
+
+const repeatString = function(text, num) {
+    if(num === 0){
+        return ''
+    }
+    else if(num<0){
+        return 'ERROR'
+    }
+    if(text === ''){
+        return ''
+    }
+    else{
+        let s1 = ""
+    while(num>0){
+        s1 = s1+text;
+        num--;
+    }
+    return s1
+}
 
 };
 
