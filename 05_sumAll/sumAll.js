a2e9913c1dea0b06276811fd0578dcaecf22e9e2
NandiniTandon21 <63950748+NandiniTandon21@users.noreply.github.com>
2025-01-09 23:30:28 +0530

    sum all

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
 
+const sumAll = function(a, b) {
+    let sum =0 
+    if(a<0 || b<0 || !(Number.isInteger(a)) || !(Number.isInteger(b)) || isNaN(a) || isNaN(b)){
+        return 'ERROR'
+    }
+   if(b>a){
+    start =a ;
+    end=b;
+   }
+   else{
+    start =b;
+    end=a;
+   }
+sum = Math.abs(((end*(end+1))/2)-start+1)
+console.log('@@sum = ', sum )
+return sum 
 };
 
 // Do not edit below this line
